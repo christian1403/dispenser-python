ebecb36d0742fb9510e72d54cd349c03b13013d2
Christian Chandra <chanchrisid@gmail.com>
2025-08-21 22:56:28 +0700

    feat: add mode prod or dev

--- app.py
+++ app.py
@@ -16,5 +16,8 @@ if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", 5000))
     debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
-    
-    app.run(host=host, port=port, debug=debug)
+    mode = os.getenv("FLASK_ENV", "production").lower()
+    if mode == "development":
+        app.run(host=host, port=port, debug=debug)
+    else:
+        app.run(debug=debug)
